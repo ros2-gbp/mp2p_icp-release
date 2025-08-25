d222cd66999a338df5c1cff9ded29920e8ba1d82
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-08-26 01:19:19 +0200

    Rebase from 'release/humble/mp2p_icp'

--- mp2p_icp/src/Matcher_Point2Plane.cpp
+++ mp2p_icp/src/Matcher_Point2Plane.cpp
@@ -1,8 +1,16 @@
-/* -------------------------------------------------------------------------
- *  A repertory of multi primitive-to-primitive (MP2P) ICP algorithms in C++
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
- * See LICENSE for license information.
- * ------------------------------------------------------------------------- */
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ A repertory of multi primitive-to-primitive (MP2P) ICP algorithms
+ and map building tools. mp2p_icp is part of MOLA.
+
+ Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: BSD-3-Clause
+*/
 /**
  * @file   Matcher_Point2Plane.cpp
  * @brief  Pointcloud matcher: point to plane-fit of nearby points
@@ -14,7 +22,6 @@
 #include <mp2p_icp/estimate_points_eigen.h>
 #include <mrpt/core/exceptions.h>
 #include <mrpt/core/round.h>
-#include <mrpt/version.h>
 
 IMPLEMENTS_MRPT_OBJECT(Matcher_Point2Plane, Matcher, mp2p_icp)
 
