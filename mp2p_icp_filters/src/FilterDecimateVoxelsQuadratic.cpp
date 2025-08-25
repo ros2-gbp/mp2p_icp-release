eaa4de4caeeee52d9a73f0e9e77bcac810ee3908
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-08-26 01:29:10 +0200

    Rebase from 'release/rolling/mp2p_icp'

--- mp2p_icp_filters/src/FilterDecimateVoxelsQuadratic.cpp
+++ mp2p_icp_filters/src/FilterDecimateVoxelsQuadratic.cpp
@@ -1,8 +1,16 @@
-/* -------------------------------------------------------------------------
- * A repertory of multi primitive-to-primitive (MP2P) ICP algorithms in C++
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
- * See LICENSE for license information.
- * ------------------------------------------------------------------------- */
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ A repertory of multi primitive-to-primitive (MP2P) ICP algorithms
+ and map building tools. mp2p_icp is part of MOLA.
+
+ Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: BSD-3-Clause
+*/
 /**
  * @file   FilterDecimateVoxelsQuadratic.cpp
  * @brief  Builds a new layer with a decimated version of an input layer.
@@ -45,7 +53,7 @@ void FilterDecimateVoxelsQuadratic::initialize(const mrpt::containers::yaml& c)
     MRPT_LOG_DEBUG_STREAM("Loading these params:\n" << c);
     params_.load_from_yaml(c);
 
-    filter_grid_.setResolution(params_.voxel_filter_resolution);
+    filter_grid_.setConfiguration(params_.voxel_filter_resolution, true);
     quadratic_reference_radius_inv_ = 1.0f / params_.quadratic_reference_radius;
 
     MRPT_END
