887dd3fe89d8343835a21fc46c0eadac5080b900
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-08-26 01:19:42 +0200

    Rebase from 'release/humble/mp2p_icp'

--- mp2p_icp/include/mp2p_icp/QualityEvaluator.h
+++ mp2p_icp/include/mp2p_icp/QualityEvaluator.h
@@ -1,8 +1,16 @@
-/* -------------------------------------------------------------------------
- *  A repertory of multi primitive-to-primitive (MP2P) ICP algorithms in C++
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
- * See LICENSE for license information.
- * ------------------------------------------------------------------------- */
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ A repertory of multi primitive-to-primitive (MP2P) ICP algorithms
+ and map building tools. mp2p_icp is part of MOLA.
+
+ Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: BSD-3-Clause
+*/
 /**
  * @file   QualityEvaluator.h
  * @brief  Matching quality evaluator (virtual base class)
@@ -17,7 +25,6 @@
 #include <mrpt/containers/yaml.h>
 #include <mrpt/rtti/CObject.h>
 #include <mrpt/system/COutputLogger.h>
-#include <mrpt/version.h>
 
 namespace mp2p_icp
 {
@@ -29,11 +36,7 @@ class QualityEvaluator : public mrpt::system::COutputLogger,
                          public mrpt::rtti::CObject,
                          public mp2p_icp::Parameterizable
 {
-#if MRPT_VERSION < 0x020e00
-    DEFINE_VIRTUAL_MRPT_OBJECT(QualityEvaluator)
-#else
     DEFINE_VIRTUAL_MRPT_OBJECT(QualityEvaluator, mp2p_icp)
-#endif
 
    public:
     struct Result
