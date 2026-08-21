11db0c6629cfab0db6d2aa10b3f9f2dbce3fcf9e
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2026-08-21 15:26:06 +0200

    Rebase from 'upstream'

--- mp2p_icp_core/mp2p_icp/include/mp2p_icp/Matcher_Point2Line.h
+++ mp2p_icp_core/mp2p_icp/include/mp2p_icp/Matcher_Point2Line.h
@@ -30,7 +30,7 @@ namespace mp2p_icp
  *
  * By default, each `local` point layer is matched against the layer with the
  * same name in the `global` map, unless specified otherwise in the base class
- * member `weight_pt2pt_layers`. Refer to example configuration YAML files for
+ * member `pt2pt_layer_matches`. Refer to example configuration YAML files for
  * example configurations.
  *
  * \ingroup mp2p_icp_grp
@@ -43,7 +43,8 @@ class Matcher_Point2Line : public Matcher_Points_Base
     Matcher_Point2Line();
 
     /** Parameters:
-     * - `distanceThreshold`: Inliers distance threshold [meters][mandatory]
+     * - `distanceThreshold`: Inliers distance threshold [meters][mandatory].
+     *   Accepts dynamic formulas, e.g. "2.0*ADAPTIVE_THRESHOLD_SIGMA".
      * - `knn`: Number of neighbors to look for [mandatory]
      * - `minimumLinePoints`: Minimum number of found points [mandatory]
      * - `lineEigenThreshold`: maximum e0/e2 and e1/e2 ratio [mandatory]
